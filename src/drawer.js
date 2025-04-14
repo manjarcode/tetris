b009b9c66058698139bd2d85784bd16a59b719ce
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2025-04-14 20:40:42 +0200

    Extracted getColor from drawer

--- src/drawer.js
+++ src/drawer.js
@@ -1,4 +1,5 @@
 import { BLOCK_SIZE, COLS, ROWS } from './constants.js'
+import { getColor, getDarkerColors } from './utils.js'
 
 export default class Drawer {
   constructor(canvas) {
@@ -46,31 +47,4 @@ export default class Drawer {
     this.canvas.fillStyle="#000000"
     this.canvas.fillText("Game Over!", 85, 376)
   }
-}
-
-function getColor(number) {
-  const colors = [
-    "#FF6B6B",
-    "#FF9F1C",
-    "#FFD23F",
-    "#17C3B2",
-    "#227C9D",
-    "#8B5CF6",
-    "#E6E6EA"
-  ]
-
-  return colors[number % colors.length]
-}
-
-function getDarkerColors(number) {
-  const borders = [
-    "#C34C4C", 
-    "#B36D14", 
-    "#B8962E", 
-    "#0F8175", 
-    "#165166", 
-    "#5A3EAB", 
-    "#A1A1A5"
-  ]
-  return borders[number % borders.length]
 }
